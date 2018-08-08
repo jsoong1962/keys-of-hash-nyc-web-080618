805a57959c23b59d0235933451c5aa843a8f2705
Johnny Soong <johnnysoong@hotmail.com>
2018-08-08 00:39:07 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,13 +2,13 @@ require 'pry'
 
 class Hash
   def keys_of(*arguments)
-    array = []
-    self.each do |animal, place|
-      if arguments.include?(place)
-        array << place
-      end
+     array = []
+  self.each do |animal, place|
+    if arguments.include?(place)
+      array << animal
     end
   end
   array
+  end
 end
 
